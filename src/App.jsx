537c45e207a1128f0644111cc6363b8d55f54aa1
inspiringsource <ab.b2k3@gmail.com>
2022-05-11 14:39:14 +0200

    +Custom Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,58 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
+function useAnimalSearch() {
+
+  const [animals, setAnimals] = useState([])
+
+  useEffect(() => {
+    const lastQuery = localStorage.getItem('lastQuery')
+    search(lastQuery)
+  }, [])
+
+  const search = async (q) => {
+    const response = await fetch(
+      'http://localhost:8080?' + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
+
+    localStorage.setItem('lastQuery', q);
+  };
+
+  return { search, animals };
+}
+
+
+
 function App() {
+  const { search, animals } = useAnimalSearch()
 
   return (
     <div>
-      
+      <h1>Quick search</h1>
+      <input 
+      type="text" 
+      placeholder="Search" 
+      onChange={(e) => search(e.target.value)} 
+      />
+
+      <ul>
+        {animals.map(animal => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+         {animals.length === 0 && 'No match found'}
+      </ul>
     </div>
+  )
+}
+
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <b>{type}</b> <u>Name:</u> {name} <u>Age:</u> {age}
+    </li>
   )
 }
 
